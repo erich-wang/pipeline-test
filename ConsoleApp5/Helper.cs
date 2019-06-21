716847cdf14088cfa6118e2c329dbc18ba5cf77e
Erich(Renyong) Wang <eriwan@microsoft.com>
2019-06-21 17:21:58 +0800

    first

---
+++ ConsoleApp5/Helper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp5
+{
+    class Helper
+    {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = Helper.Add(3, 3);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
